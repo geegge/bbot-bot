ef5737c9e71700454d8decfcf8526c4873305a6e
guenther <email@suepergue.de>
2019-12-28 15:19:45 +0100

    adds delay for dev env startup

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const bot = require("bbot");
 
 require("./src/main.js");
+const dotEnvProductionEnv = process.env.NODE_ENV || false;
 
-bot.start();
+//in dev bot has to wait for startup of local rocketchat enviroment
+if (dotEnvProductionEnv === "production") {
+  bot.start();
+} else {
+  setTimeout(() => {
+    bot.start();
+  }, 60000);
+}
